6dda9f01872c2c441b5ee728807b2480f34af3b7
jihee <esder1310@naver.com>
2024-05-11 17:00:47 +0900

    feat:redux 코드 다시 작성(userSlice, store, App.js)

--- miniproject/src/redux/store.js
+++ miniproject/src/redux/store.js
@@ -1,12 +1,9 @@
 import { configureStore} from '@reduxjs/toolkit';
 import userSlice from './userSlice';
-import sessionSlice from './sessionSlice';
-import authSlice from './authSlice';
 
 const store=configureStore({
     reducer:{
-        // user:userSlice,
-        auth:authSlice
+        user:userSlice,
     },
 });
 
