00b4249953538108ffafe07bb1621e0b8899172e
jihee <esder1310@naver.com>
2024-05-13 09:56:59 +0900

    feat:Main, Login, Join page에 input value localStorage에 저장하는 코드 완성

--- miniproject/src/pages/Main.js
+++ miniproject/src/pages/Main.js
@@ -1,17 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import Cookies from 'js-cookie';
 
 import Header from '../components/Header';
-import Star from '../components/Star';
 import Stars from '../components/Stars';
 import Footer from '../components/Footer';
 import Circle from '../components/Circle';
 
-import RandomStars from '../components/RandomStars';
-
 import CheckLoginStatus from '../cookie/CheckLoginStatus';
-
 
 const Container=styled.div`
     display:flex;
@@ -28,8 +24,27 @@ const Container=styled.div`
 `;
 
 const Main = () => {
+    // 쿠키
     const [isLoggedIn, setIsLoggedIn]=useState(Cookies.get('isLoggedIn')==='true');
+
+    // 로컬스토리지
+    const [inputValue,setInputValue]=useState('');
+
+    useEffect(()=>{
+        const storedValue=localStorage.getItem('inputValue');
+        if(storedValue){
+            setInputValue(storedValue);
+        }
+    },
+    // 빈 배열을 전달하여 페이지가 로드될 때 한 번만 실행되도록함
+    []);
     
+    // input 값 변경될 때마다 상태 업데이트 및 로컬 스토리지에 저장
+    const handleChange=(event)=>{
+        const value=event.target.value;
+        setInputValue(value);
+        localStorage.setItem('inputValue',value);
+    };
 
     // 로그인 버튼 클릭 시 로그인 상태를 변경하고 쿠키를 설정 -> 이거는 로그인 페이지에서 선언하기
     const handleLogin=()=>{
@@ -42,19 +57,25 @@ const Main = () => {
         Cookies.remove('isLoggedIn');
     }
 
-    // 별 위치,크기,시간 함수
-    const randomNumber = Math.floor(Math.random()*1301);
-    const sizeRandom=Math.floor(Math.random()*7)+7;
-    const timeRandom=Math.floor(Math.random()*0.1)+0.5
     return(
         <>
             <Container>
-                <Header isLoggedIn={isLoggedIn} onLogin={handleLogin} onLogout={handleLogout}/>
+                <Header 
+                    isLoggedIn={isLoggedIn} 
+                    onLogin={handleLogin} 
+                    onLogout={handleLogout}
+                    
+                    />
+                <input
+                    type='text'
+                    value={inputValue}
+                    onChange={handleChange}
+                    placeholder='test input localStorage'
+                />
+                <p>Test Input value : {inputValue}</p>
                 <Circle styled={{ zIndex: 10 }}/>
-                {/* <Star x={randomNumber} y={randomNumber} size={sizeRandom} time={timeRandom}/> */}
                 <Stars />
                 <Footer />
-                {/* <RandomStars /> */}
                 <CheckLoginStatus />
             </Container> 
         </>
