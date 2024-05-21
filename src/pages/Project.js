0304785cd15d4b93c734e2b43ffed82d8345bc64
kellylee23 <dmstj4632@hufs.ac.kr>
2024-05-21 23:09:01 +0900

    디테일페이지 api 수정

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -1,4 +1,4 @@
-import React, {useState, useEffect}from 'react';
+import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
@@ -143,19 +143,19 @@ const Project=()=>{
                         </Link>
                     </Buttondiv>
                     <ImageContainerSet>
-                        <ImageContainers ></ImageContainers>
-                        <ImageContainers ></ImageContainers>
-                        <ImageContainers ></ImageContainers>  
+                        <ImageContainers></ImageContainers>
+                        <ImageContainers></ImageContainers>
+                        <ImageContainers></ImageContainers>  
                     </ImageContainerSet>
-                    <ImageContainerSet >
-                        <ImageContainers ></ImageContainers>
-                        <ImageContainers ></ImageContainers>
-                        <ImageContainers ></ImageContainers>  
-                    </ImageContainerSet>                          
-                    <ImageContainerSet >
-                        <ImageContainers ></ImageContainers>
-                        <ImageContainers ></ImageContainers>
-                        <ImageContainers ></ImageContainers>  
+                    <ImageContainerSet>
+                        <ImageContainers></ImageContainers>
+                        <ImageContainers></ImageContainers>
+                        <ImageContainers></ImageContainers>  
+                    </ImageContainerSet>                        
+                    <ImageContainerSet>
+                        <ImageContainers></ImageContainers>
+                        <ImageContainers></ImageContainers>
+                        <ImageContainers></ImageContainers>  
                     </ImageContainerSet>                                                      
                 </Projectdiv>
                 <Footer></Footer>
